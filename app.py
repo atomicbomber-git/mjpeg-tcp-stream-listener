4c63217f5366eb453a650143e26864fc940b6fa6
James Patrick Keegan <atomicbomber.email@gmail.com>
2020-02-29 10:34:39 +0700

    Refactoring.

--- app.py
+++ app.py
@@ -1,125 +1,59 @@
-import socket
+import threading
 
 from flask import Flask, Response
+from stream.listener import TCPStreamImageListener
+from dotenv import load_dotenv
+import waitress
+import os
+
+SOURCE_DISCONNECTED_IMAGE = "stream_source_disconnected.png"
+
+# Initialize environment variables from ./.env file. Example can be found at ./.env.example
+load_dotenv()
+
 
 app = Flask(__name__)
 
-import socket
-import threading
-
-MAX_TOLERATED_EMPTY_MESSAGES = 100
-DATA_DELIMITER_SYMBOL = b':::'
-MODE_MESSAGE_LEN = 'INTERPRET_MESSAGE_LEN'
-MODE_IMAGE = 'INTERPRET_IMAGE'
-MODE_SEEK_MARKER = 'INTERPRET_SEEK_MARKER'
-
-HOST = "127.0.0.1"
-PORT = 9999
-
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-socket.bind((HOST, PORT))
-socket.listen()
-socket.setblocking(False)
-
-current_interpret_mode = MODE_SEEK_MARKER
-message_length_digits = b''
-image_message_len = -1
-back_buffer = []
-
-must_reconnect = True
-
-connection = None
+image_data = open(SOURCE_DISCONNECTED_IMAGE, "rb").read()
 
 
-image_data = open("ssd.png", "rb").read()
-image_data_lock = threading.Lock()
+def listen_to_updates():
+    global image_data
+
+    def on_image_update(new_image_data):
+        global image_data
+        image_data = new_image_data
+
+    TCPStreamImageListener(
+        listen_host=os.getenv("LISTEN_HOST"),
+        listen_port=int(os.getenv("LISTEN_PORT")),
+        on_image_update=on_image_update
+    ).listen()
 
 
-class DisconnectedError(Exception):
-    pass
-
-def eat_message(connection, expected_message_length):
-    global must_reconnect
-
-    message_buffer = b''
-    empty_message_count = 0
-
-    while len(message_buffer) < expected_message_length:
-        if len(back_buffer) > 0:
-            message_buffer += back_buffer.pop()
-        else:
-            temp = connection.recv(1)
-
-            if temp == b'':
-                empty_message_count += 1
-
-            if empty_message_count > MAX_TOLERATED_EMPTY_MESSAGES:
-                raise DisconnectedError()
-
-            message_buffer += temp
-
-    return message_buffer
-
-def update_image():
-    global connection, must_reconnect, message_length_digits, current_interpret_mode, image_data, image_message_len
-
-    while True:
-        try:
-            if must_reconnect:
-                connection, address = socket.accept()
-                must_reconnect = False
-
-            if current_interpret_mode == MODE_MESSAGE_LEN:
-                message = eat_message(connection, 1)
-
-                if message.isdigit():
-                    message_length_digits += message
-                else:
-                    back_buffer.append(message)
-
-                    try:
-                        image_message_len = int(message_length_digits)
-                        current_interpret_mode = MODE_IMAGE
-                    except ValueError:
-                        current_interpret_mode = MODE_SEEK_MARKER
-
-                    message_length_digits = b''
-            elif current_interpret_mode == MODE_SEEK_MARKER:
-                buffer = b''
-
-                while len(buffer) < len(DATA_DELIMITER_SYMBOL):
-                    buffer += eat_message(connection, 1)
-
-                if buffer == DATA_DELIMITER_SYMBOL:
-                    current_interpret_mode = MODE_MESSAGE_LEN
-
-            elif current_interpret_mode == MODE_IMAGE:
-                image_data = eat_message(connection, image_message_len)
-                current_interpret_mode = MODE_SEEK_MARKER
-            else:
-                raise Exception("INVALID_STATE, mode {} is unknown.", current_interpret_mode)
-        except (IOError, DisconnectedError):
-            must_reconnect = True
-
-stream_thread = threading.Thread(target=update_image)
-stream_thread.daemon = True
+stream_thread = threading.Thread(target=listen_to_updates)
+stream_thread.setDaemon(True)
 stream_thread.start()
+
 
 @app.route('/')
 def video_feed():
-    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-if __name__ == '__main__':
-    app.run()
-
+    return Response(gen_image_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def to_frame(binary_data):
     return (b'--frame\r\n'
             b'Content-Type: image/jpeg\r\n\r\n' + binary_data + b'\r\n')
 
-def gen():
+
+def gen_image_frame():
     while True:
         image = image_data
         yield to_frame(image)
+
+
+waitress.serve(
+    app,
+    host=os.getenv("SERVE_HOST"),
+    port=os.getenv("SERVE_PORT")
+)
+
---
+++ stream/listener.py
@@ -0,0 +1,104 @@
+import socket
+from typing import Callable, ClassVar
+
+MAX_TOLERATED_EMPTY_MESSAGES = 100
+DATA_DELIMITER_SYMBOL = b':::'
+MODE_MESSAGE_LEN = 'INTERPRET_MESSAGE_LEN'
+MODE_IMAGE = 'INTERPRET_IMAGE'
+MODE_SEEK_MARKER = 'INTERPRET_SEEK_MARKER'
+
+
+class DisconnectedError(Exception):
+    pass
+
+
+class TCPStreamImageListener:
+    def __init__(
+            self,
+            listen_host: str,
+            listen_port: int,
+            on_image_update: Callable
+    ) -> None:
+
+        self.socket = None
+        self.connection = None
+        self.must_reconnect = True
+        self.listen_host = listen_host
+        self.listen_port = listen_port
+
+        self.on_image_update: ClassVar[Callable] = on_image_update
+        self.current_interpret_mode = MODE_SEEK_MARKER
+        self.message_length_digits = b''
+        self.image_message_len = -1
+
+        self.message_back_buffer = []
+
+        self.init_socket()
+
+    def init_socket(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.listen_host, self.listen_port))
+        self.socket.listen()
+        self.socket.setblocking(False)
+
+    def eat_message(self, expected_message_length):
+        message_buffer = b''
+        empty_message_count = 0
+
+        while len(message_buffer) < expected_message_length:
+            if len(self.message_back_buffer) > 0:
+                message_buffer += self.message_back_buffer.pop()
+            else:
+                temp = self.connection.recv(1)
+
+                if temp == b'':
+                    empty_message_count += 1
+
+                if empty_message_count > MAX_TOLERATED_EMPTY_MESSAGES:
+                    raise DisconnectedError()
+
+                message_buffer += temp
+        return message_buffer
+
+    def listen(self):
+        while True:
+            try:
+                if self.must_reconnect:
+                    self.connection, _ = self.socket.accept()
+                    self.must_reconnect = False
+
+                if self.current_interpret_mode == MODE_MESSAGE_LEN:
+                    message = self.eat_message(expected_message_length=1)
+
+                    if message.isdigit():
+                        self.message_length_digits += message
+                    else:
+                        self.message_back_buffer.append(message)
+
+                        try:
+                            self.image_message_len = int(self.message_length_digits)
+                            self.current_interpret_mode = MODE_IMAGE
+                        except ValueError:
+                            self.current_interpret_mode = MODE_SEEK_MARKER
+
+                        self.message_length_digits = b''
+                elif self.current_interpret_mode == MODE_SEEK_MARKER:
+                    buffer = b''
+
+                    while len(buffer) < len(DATA_DELIMITER_SYMBOL):
+                        buffer += self.eat_message(expected_message_length=1)
+
+                    if buffer == DATA_DELIMITER_SYMBOL:
+                        self.current_interpret_mode = MODE_MESSAGE_LEN
+
+                elif self.current_interpret_mode == MODE_IMAGE:
+
+                    self.on_image_update(
+                        self.eat_message(expected_message_length=self.image_message_len)
+                    )
+
+                    self.current_interpret_mode = MODE_SEEK_MARKER
+                else:
+                    raise Exception("INVALID_STATE, mode {} is unknown.", self.current_interpret_mode)
+            except (IOError, DisconnectedError):
+                self.must_reconnect = True
